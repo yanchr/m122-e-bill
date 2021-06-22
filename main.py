1857b5e6f26c5a35c8d609bf7acc031c3b976dbf
Yanick <yanick.christen@gmail.com>
2021-06-22 11:42:55 +0200

    added receipt

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ ftpKundensystemService.grabFile('/out/AP18dChristen', 'rechnung21003.data')
 parsedText = dataToCsv.parse_csv('temp-files/invoices/data/rechnung21003.data')
 csvToTxt.parse_to_txt(parsedText)
 ftpZahlsystemService.placeFile('/in/AP18dChristen', 'invoice.txt')
-#ftpZahlsystemService.grabReceipts('/out/AP18dChristen')
+ftpZahlsystemService.grabReceipts('/out/AP18dChristen')
 
 
